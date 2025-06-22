c35b336a855ed097db6d64a93eac66919d356f4d
Pavan Vasam <pavanvasam7@gmail.com>
2025-06-22 19:19:48 +0530

    created homepage

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Homepage from './pages/homepage'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Homepage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+function Homepage(){
+    return(
+        <div>
+            <h1>Homepage</h1>
+            <div>
+                {/* <Profile/>
+                <Projects/> */}
+            </div>
+        </div>
+    )
+}
+export default Homepage
