ab5ce10ca9a27a78a2c20988d93c2440382f48a1
Pavan Vasam <pavanvasam7@gmail.com>
2025-06-23 11:32:39 +0530

    added education and experience section

---
+++ src/components/sections/EducationAndExperience.jsx
@@ -0,0 +1,29 @@
+import { FaLandmark } from "react-icons/fa6";
+
+function EducationAndExperience(){
+    return(
+        <section id="education-and-experience">
+            <div>
+                <h4>
+                    <FaLandmark/>
+                    Education
+                </h4>
+                {info.education.map((edu,index)=>{
+                    return(
+                        <div key={index}>
+                        <div>
+                            <h3>{edu.duration}</h3>
+                        </div>
+                        <div>
+                            <div>
+                                <div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
+    )
+}
