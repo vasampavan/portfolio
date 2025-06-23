2b302faa92447f24fbbba1f51cb6b28aa5c772a3
Pavan Vasam <pavanvasam7@gmail.com>
2025-06-23 11:35:49 +0530

    added skills section

--- src/components/sections/EducationAndExperience.jsx
+++ src/components/sections/EducationAndExperience.jsx
@@ -27,3 +27,4 @@ function EducationAndExperience(){
         </section>
     )
 }
+export default EducationAndExperience
---
+++ src/components/sections/Skills.jsx
@@ -0,0 +1,17 @@
+import { FaHtml5 } from "react-icons/fa6";
+
+function Skills(){
+    return(
+        <section id="skills">
+            <h4>
+                Technologies I Use.
+            </h4>
+            <div>
+                <span>
+                    <FaHtml5/>HTML
+                </span>
+            </div>
+        </section>
+    )
+}
+export default Skills
