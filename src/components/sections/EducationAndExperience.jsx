f88c482659d0971b37e8b22e0a20b330b7f3b7c9
Pavan Vasam <pavanvasam7@gmail.com>
2025-06-23 14:15:38 +0530

    added certificates section

--- src/components/sections/EducationAndExperience.jsx
+++ src/components/sections/EducationAndExperience.jsx
@@ -17,13 +17,81 @@ function EducationAndExperience(){
                         <div>
                             <div>
                                 <div>
+                                    <div></div>
                                 </div>
                             </div>
+                        <div>
+                            <h3>
+                                <img src={edu.image} alt="school logo" />
+                                <div>
+                                    {edu.school}
+                                    <p>{edu.degree}</p>
+                                </div>
+                            </h3>
+                        </div>
                         </div>
                         </div>
                     )
                 })}
+                <h4>
+                   <PiCertificateFill/>
+                   Certificates
+                </h4>
+                <div>
+                    <div>
+                        <div>
+                            {info.certificates.map((cert,index)=>{
+                                return(
+                                    <div key={index}>
+                                        {cert.icon==="ibm"?(
+                                            <SiIbm/>
+                                        ):cert.icon==="google"?(
+                                            <FcGoogle/>
+                                        ):(
+                                            <SiHackerrank/>
+                                        )}
+                                        <div>
+                                            <div>
+                                                <a href={cert.link}>
+                                                    <p>{cert.title}</p>
+                                                    <p>{cert.description}</p>
+                                                </a>
+                                                <span>
+                                                    Open Certificate
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <button>
+                        <span>
+                            <svg>
+                                <path></path>
+                            </svg>
+                        </span>
+                        <span>Previous</span>
+                    </button>
+                    <button>
+                        <span>Next</span>
+                        <span>
+                            <svg>
+                                <path></path>
+                            </svg>
+                        </span>
+                    </button>
+                    <div>
+                        {info.certificates.map((cert,index)=>{
+                            return(
+                                <span key={index}></span>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
+            
         </section>
     )
 }
