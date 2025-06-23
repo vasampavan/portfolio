d3eeedb94f24bf792a77573c642ebf97f4afdeb2
Pavan Vasam <pavanvasam7@gmail.com>
2025-06-23 09:47:53 +0530

    updated profile section

--- src/components/sections/Profile.jsx
+++ src/components/sections/Profile.jsx
@@ -1,21 +1,25 @@
-
-import profileimg from '../../assets/IMG20250425140151 (1).jpg'
+import { IoIosArrowForward } from "react-icons/io"
 function Profile(){
     return(
         <section>
             <div>
                 <div>
                     <div>
-                        <h2>Full-Stack Developer</h2>
-                        <h1>Pavan Vasam</h1>
-                        <p>
- Experienced mentor specializing in Data Structures and Algorithms (DSA) with Smart Interviews. Eager to apply expertise in DSA
- and web development to real-world projects. Possesses strong skills in mentoring and a strong foundation in web development
- technologies</p>
+                        <h2>{info.main.role}</h2>
+                        <h1>{info.main.name}</h1>
+                        <p>{info.main.description}</p>
                     
+                        <div>
+                            <a href="#projects">Projects</a>
+                            <a href="#contact">
+                                <span>Contact</span>
+                                <IoIosArrowForward/>
+                            </a>
+                        </div>
+
                     </div>
                     <div>
-                      <img src={profileimg} alt="" />
+                      <img src={info.main.photo} alt="" />
                     </div>
                 </div>
             </div>
