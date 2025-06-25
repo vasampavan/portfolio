3cf67e1ddd68e977e7902cf4a5b46beae88c41e5
Pavan Vasam <pavanvasam7@gmail.com>
2025-06-25 10:21:59 +0530

    added styling for skills section

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,13 +1,14 @@
 import { FaHtml5 } from "react-icons/fa6";
+import '../styles/skills.css'
 function Skills(){
     return(
-        <section id="skills">
-            <h4>
+        <section id="skills" className="skills">
+            <h4 className="title">
                 Technologies I Use.
             </h4>
-            <div>
-                <span>
-                    <FaHtml5/>HTML
+            <div className="grid">
+                <span className="item">
+                    <FaHtml5 className="icon"/>HTML
                 </span>
             </div>
         </section>
