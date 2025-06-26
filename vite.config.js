33fa2720c1710b49c352bfab6db4b3e199cd6f8b
Pavan Vasam <pavanvasam7@gmail.com>
2025-06-26 13:18:43 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"portfolio",
+  base:process.env.VITE_BASE_PATH || "/portfolio",
   plugins: [react()],
 })
